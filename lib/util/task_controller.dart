d6a643c3f2077869e3c218d82d0d5161a493124c
Spuddle-Z <Zhang_Yizhou@sjtu.edu.cn>
2025-01-18 20:10:21 +0800

    完成周期性任务生成器

--- lib/util/task_controller.dart
+++ lib/util/task_controller.dart
@@ -5,9 +5,47 @@ import 'task_model.dart';
 class TaskController extends GetxController {
   Rx<Box<Task>> taskBox = Hive.box<Task>('tasks').obs;
 
+  @override
+  void onInit() {
+    super.onInit();
+    generateTask();
+  }
+
+  void generateTask() {
+    // 计算周期性任务的下个截止日期
+    DateTime getNextDate(DateTime date, String recurrence) {
+      if (recurrence == '每天') {
+        return date.add(const Duration(days: 1));
+      } else if (recurrence == '每周') {
+        return date.add(const Duration(days: 7));
+      } else if (recurrence == '每月') {
+        return DateTime(date.year, date.month + 1, date.day);
+      } else {
+        return DateTime(date.year + 1, date.month, date.day);
+      }
+    }
+    
+    // 判断是否生成新任务
+    bool ifGenerate(bool done, DateTime date) {
+      return done || date.isBefore(DateTime.now()) || date.isAtSameMomentAs(DateTime.now());
+    }
+
+    List<dynamic> keys = taskBox.value.keys.where((k) => taskBox.value.get(k)!.taskRecurrence != '不重复').toList();
+    for (int i = 0; i < keys.length; i++) {
+      Task task = taskBox.value.get(keys[i])!;
+      while (ifGenerate(task.taskDone, task.taskDate!)) {
+        Task newTask = Task.copy(task);
+        newTask.taskRecurrence = '不重复';
+        taskBox.value.add(newTask);
+        task.taskDate = getNextDate(task.taskDate!, task.taskRecurrence);
+      }
+    }
+  }
+
   // 添加任务
   void addTask(Task newTask) {
     taskBox.value.add(Task.copy(newTask));
+    generateTask();
     taskBox.refresh();
   }
 
@@ -16,6 +54,7 @@ class TaskController extends GetxController {
     final task = taskBox.value.get(key);
     task!.taskDone = !task.taskDone;
     taskBox.value.put(key, task);
+    generateTask();
     taskBox.refresh();
   }
 
